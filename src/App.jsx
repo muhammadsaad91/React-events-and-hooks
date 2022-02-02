53f1477457624564251cbf1f6e494c1d7b908360
muhammadsaad91 <saadattari91@gmail.com>
2022-02-02 14:23:52 +0500

    Final hooks app

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 
 const App=()=>{
     
-    const yello="yellow"
+    const yello="grey"
     const [bg,setbg]= useState(yello);
     const [bgn,setbgn]=useState('saad')
     const name=()=>{
          setbgn("Muhammad SaAd")
     };
     const bgchange=()=>{
-      const purpl="purple"
+      const purpl="aqua"
          setbg(purpl)
     }; 
 return(
     <>
+    <div className="cards">
+    <ul><li><h1>Single Click To Change Back grond Color</h1></li>
+    <li><h1>Double Click To Change Button Name</h1></li>
+    </ul>
+    </div>
     <div style={{ backgroundColor : bg}}className="card">
+    
+    
+    
     <button onClick={bgchange} onDoubleClick={name}>{bgn} </button>
     </div>
     </>
